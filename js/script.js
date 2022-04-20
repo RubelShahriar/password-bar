367552a1c39a060101f7fc9c324bee214404a0f7
RubelShahriar <mr.rubelshahriar@gmail.com>
2022-04-20 12:03:52 +0600

    update css

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const password = document.getElementById('password')
+const password_toggle = document.getElementById('password-toggle')
+
+function passwordToggle() {
+    if( password.type === 'password'){
+        password.type = 'text'
+        password_toggle.classList.remove("fa-eye-slash")
+        password_toggle.classList.add("fa-eye")
+    }else{
+        password.type = 'password'
+        password_toggle.classList.add('fa-eye')
+        password_toggle.classList.remove('fa-eye-slash')
+    }
+}
